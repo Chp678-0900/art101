a5f67ad93473ab96c0970f2daf8fb810fa060c15
Chp678-0900 <ceuherna@ucsc.edu>
2025-04-21 16:59:09 -0700

    Update

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,10 +1,20 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Lab4: Checkers Pseudocode.js
+// a pseudcode for the game, checkers
+// Author: Cesar and Sam
+// Date: April 2025
 
-// Constants
+// To print the board for each turn
+// Add all the pieces for the entire game.
+// Players decide who team is red and team black
+// Flip a coin to decide who will go first.
+// Whoever wins the coin flip is able to move their checker piece first diagonally. 
+// The second will then make the next diagonal move. 
+// Repeat turns to capture your opponet's checker pieces by jumping over them. 
+  // If a player's checker reaches the end of the board it will become a king piece.
+  // A King Piece wiil be able to move diagonally backwards and forewards.
+// A winning player is decided once a player captures all of the checker pieces of the opposing player.
 
-// Functions
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
