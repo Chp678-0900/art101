51b86e4893c58fe4847434d811f33a7a7394d2ab
Chp678-0900 <ceuherna@ucsc.edu>
2025-05-08 12:02:01 -0700

    lab 8

---
+++ lab8/js/lab.js
@@ -0,0 +1,29 @@
+// Lab 8 - Anon Functions
+// Author: Cesar Hernandez
+// Date: May 7th 2025
+
+// Array
+let array = [4,18,250,500,1000]
+
+// Functions
+quickMath(6)
+
+// Function Math 1
+function quickMath(numb) {
+  return numb * 4 + 1
+}
+
+//Anonymous Function
+array.map(function(x){
+  return x + 6 * 8;
+})
+  
+// Array Map 1
+var Key = array.map(quickMath);
+
+
+// Console Log Outputs
+console.log("My Selected Numbers: ", array);
+console.log("6 will be multiplied by 4 then add 1. ", quickMath(6));
+console.log("I want to PEMDAS on the selected numbers: ", Key);
+console.log("Be anonymous with the array", anon);
