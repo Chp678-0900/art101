b32e929dbb8710491c963c0268ff33bd5a00e747
Chp678-0900 <ceuherna@ucsc.edu>
2025-05-12 16:51:22 -0700

    lab9

---
+++ lab9/js/lab.js
@@ -0,0 +1,37 @@
+// Lab 9- Jqueries and Libaries (Not the libary book kind)
+// Author: Cesar Hernandez
+// Date: 5-12-2025
+
+// Challenge Section Button
+$("#challenge").apend("<'button id= button-challenge'>Big Box Button</button>");
+$("#button-challenge").click(function(){
+  $("#challenge").toggleClass("special");
+});
+
+// Problem Section Button
+$("#problems").apend("<'button id= button-problems'>Big Box Button</button>");
+$("#button-problems").click(function(){
+  $("#problems").toggleClass("special");
+});
+
+// Results Section Button
+$("#results").apend("<'button id= button-results'>Big Box Button</button>");
+$("#results").click(function(){
+  $("#results").toggleClass("special");
+})
+
+// Function
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+  // some code here
+  // return results;
+}
+
+function main() {
+  console.log("Main function started.");
+  // the code that makes everything happen
+}
+
+// let's get this party started
+main();
