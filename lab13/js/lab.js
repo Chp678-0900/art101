a0a1599edad4a61af664aae38dbf91eaf3b94740
Chp678-0900 <ceuherna@ucsc.edu>
2025-05-26 19:46:08 -0700

    lab 13

---
+++ lab13/js/lab.js
@@ -0,0 +1,38 @@
+// Lab 13- Loops
+// Author: Cesar Hernandez
+// Date:5-26-2025
+
+// FizzBuzzBoom(but Trail Mix)
+function bagTrailMix() {
+  let output = "";
+
+for(let mix = 1; mix <= 230; mix++){
+  let result = ""; 
+
+  if(mix % 4 === 0 && mix % 6 === 0) {
+    result = "Grains";
+  }
+
+  else if(mix % 6 ===0){
+    result = "Raisins";
+  }
+
+  else if(mix % 7 === 0){
+    result = "Granola";
+  }
+
+  else if(mix % 4 ===0){
+    result = "M&M";
+  }
+
+  else{
+    result = mix;
+  }
+
+  output += mix + ": " + result + "<br>";
+ }
+ return output;
+}
+
+// Outputting into the webpage
+$("#output").html(bagTrailMix());
