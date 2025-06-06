b34791f93d81db2fa60e5601f58965d2f17f3f9f
Chp678-0900 <ceuherna@ucsc.edu>
2025-06-05 21:20:39 -0700

    lab 15

---
+++ lab16/js/lab.js
@@ -0,0 +1,69 @@
+// index.js - Lab 16 Json and API
+// Author: Cesar Hernandez
+// Date: 06-04-2025
+
+// Constants
+
+// Functions
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+  // some code here
+  // return results;
+}
+
+function main() {
+  console.log("Main function started.");
+  // the code that makes everything happen
+}
+
+// let's get this party started
+main();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Using the core $.ajax() method
+$.ajax({
+  // The URL for the request (from the api docs)
+  url: "https://xkcd.com/614/info.0.json",
+  // The data to send (will be converted to a query string)
+  data: { 
+          // here is where any data required by the api 
+          //   goes (check the api docs)
+          id: 614,
+          api_key: "woodpecker",
+        },
+  // Whether this is a POST or GET request
+  type: "GET",
+  // The type of data we expect back
+  dataType : "json",
+  // What do we do when the api call is successful
+  //   all the action goes in here
+  success: function(data) {
+      // do stuff
+      console.log(data);
+  },
+  // What we do if the api call fails
+  error: function (jqXHR, textStatus, errorThrown) { 
+      // do stuff
+      console.log("Error:", textStatus, errorThrown);
+  }
+})
